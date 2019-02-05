a2974c85e231b055dc96215c3f75f2f54c492aa9
silvercodes <silver.itstep@gmail.com>
2019-02-05 23:36:06 +0200

    avral

--- Client/Admin/AdminPanel.xaml.cs
+++ Client/Admin/AdminPanel.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -16,30 +18,63 @@ using Client.AdminService;
 
 namespace Client.Admin
 {
-    public partial class AdminPanel : Window
+    public partial class AdminPanel : Window, INotifyPropertyChanged
     {
-	    private DLAdmin _dlAdmin;
-		private ObservableCollection<Bus> _buses;
 
-        public AdminPanel()
-        {
-            InitializeComponent();
-			InitFunction();
-        }
+		//private ObservableCollection<Bus> _buses;
 
-        private void ExitButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
+		//      public AdminPanel()
+		//      {
+		//          InitializeComponent();
+		//	InitFunction();
+		//      }
 
-        private void InitFunction()
-        {
-			_dlAdmin = new DLAdmin();
+		//      private void ExitButton_Click(object sender, RoutedEventArgs e)
+		//      {
+		//          this.Close();
+		//      }
+
+		//      private void InitFunction()
+		//      {
+		//	_dlAdmin = new DLAdmin();
+		//	_buses = new ObservableCollection<Bus>();
+		//}
+
+		private DLAdmin _dlAdmin;
+	    private ObservableCollection<Bus> _buses; 
+		public event PropertyChangedEventHandler PropertyChanged;
+
+	    public AdminPanel()
+	    {
+			InitializeComponent();
+		    DataContext = this;
 			_buses = new ObservableCollection<Bus>();
+	    }
+
+	    public ObservableCollection<Bus> Buses
+	    {
+		    get { return _buses; }
+		    set
+		    {
+			    _buses = value;
+			    OnPropertyChanged(nameof(Buses));
+		    }
+	    }
+
+		private void ExitButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
 		}
 
 
 
 
-	}
+
+
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+	    {
+		    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	    }
+    }
 }
