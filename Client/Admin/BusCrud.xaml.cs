a270974a1550541ad7598bba5cb330e7621b2c69
Nickolay <niko.shilyaev@gmail.com>
2019-02-07 00:13:23 +0200

    takoe

---
+++ Client/Admin/BusCrud.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client.Admin
+{
+    /// <summary>
+    /// Логика взаимодействия для BusCrud.xaml
+    /// </summary>
+    public partial class BusCrud : Window
+    {
+        public BusCrud()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddBusWindow abw = new AddBusWindow();
+            abw.Show();
+            this.Close();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
