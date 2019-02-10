76df8aeef2b9abbfb05dbb740f133ec9727a44b8
heavyNS <niko.shilyaev@gmail.com>
2019-02-10 11:26:57 +0200

    .

---
+++ Client/Admin/Trips/CrudTrips.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client.Admin.Trips
+{
+    /// <summary>
+    /// Логика взаимодействия для CrudTrips.xaml
+    /// </summary>
+    public partial class CrudTrips : Window
+    {
+        public CrudTrips()
+        {
+            InitializeComponent();
+        }
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddTrips at = new AddTrips();
+            at.Show();
+        }
+    }
+}
